fde1e96cbc9af7d9fb8a8d1ae1cd70e625b32b13
rrwang <rita1218@126.com>
2024-03-18 16:23:04 +0800

    chore: restart

--- packages/sample-editor/__tests__/sample-editor.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const sampleEditor = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(sampleEditor(), 'Hello from sampleEditor');
-console.info('sampleEditor tests passed');
--- packages/sample-editor/lib/sample-editor.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = sampleEditor;
-
-function sampleEditor() {
-  return 'Hello from sampleEditor';
-}
